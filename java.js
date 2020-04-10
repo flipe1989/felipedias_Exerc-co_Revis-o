f8210973c8e95386d4757916742c55bdfd3c844d
flipe1989 <60761146+flipe1989@users.noreply.github.com>
2020-04-09 21:32:13 -0300

    Add files via upload
    upload de arquivos

---
+++ java.js
@@ -0,0 +1,27 @@
+alert("Aula de JavaScript");
+var nome = "felipe"
+var numero = 0;
+
+var cont = 0;
+while(cont <=4){
+    alert("FELIPE");
+    cont = cont + 1;
+}
+do{
+    alert("Dentro da estrutura DO");
+    alert(cont);
+    cont++;
+}while(cont<=8)
+
+
+//exibir 5 vezes o nome
+for(var x=0;x<=5;x++){
+    alert("FELIPE " );
+}
+
+int tabuada = 5; // tabuada do 5
+for(x = 0; x <= 10; x++)
+   System.out.print("Tabuada do: " + tabuada)
+   System.out.print(x + " x " + tabuada + " = ");
+   System.out.println(x * tabuada);
+}
